ff1656e2d75c94e0ac79f5f3df13d72a85e63fed
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-07-16 13:57:32 +0530

    Initiate server

---
+++ server/database/ConnectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then((data) => {
+      console.log("MongoDB connected with " + data.connection.host);
+    });
+};
+
+export default connectDB;
