6a2bbf2736e3c1962f4e133f2fd2137e58531a04
Cenote <himangshukalita131@gmail.com>
2023-07-18 02:14:49 +0530

    update register styling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,12 +6,44 @@ const Register = () => {
   return (
     <main className={styles.formContainer}>
       <form action="" className={styles.container}>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button className={styles.btn}>Sign Up</button>
+        <div className={styles.formGroup}>
+          <label htmlFor="name" className={styles.label}>
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={styles.input}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className={styles.input}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password" className={styles.label}>
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className={styles.input}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit" className={styles.btn}>
+          Sign Up
+        </button>
         <h4>Or</h4>
-        <Link to={'/login'}>login</Link>
+        <Link to={'/login'}>Login</Link>
       </form>
     </main>
   )
