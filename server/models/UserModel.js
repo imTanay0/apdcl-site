cb034e0cd284bdf287c33b6d7ace4c6d7a095ab5
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-07-17 15:26:06 +0530

    Update User Model

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,4 +15,6 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-})
+});
+
+export default mongoose.model("User", userSchema);
