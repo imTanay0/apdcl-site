49dc94e022914853047db9407dda69f0e7ec61c9
Cenote <himangshukalita131@gmail.com>
2023-07-19 15:48:37 +0530

    fix content of homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
-import Content from './pages/Content'
 import Contact from './pages/Contact'
 import Register from './pages/Register'
 import Login from './pages/Login'
@@ -12,13 +11,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path='/content' element={<Content />} /> */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-      <Content />
     </>
   )
 }
