54aaca244650049fb9129262924124afa36c645f
Cenote <himangshukalita131@gmail.com>
2023-07-19 15:31:24 +0530

    resolve errors in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from "./pages/Home"
-import Content from './pages/Content';
 import Home from './pages/Home'
+import Content from './pages/Content'
 import Contact from './pages/Contact'
 import Register from './pages/Register'
 import Login from './pages/Login'
@@ -12,17 +11,15 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          {/* <Route path='/content' element={<Content />} /> */}
           <Route path="/" element={<Home />} />
+          {/* <Route path='/content' element={<Content />} /> */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-      <Content/>
+      <Content />
     </>
-    
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import styles from "../css/Home.module.css";
+import styles from '../css/Home.module.css'
 
 const Home = () => {
   return (
@@ -12,7 +12,9 @@ const Home = () => {
           />
           <div className={styles.headerContent}>
             <h1 className={styles.fir}>অসম শক্তি বিতৰণ কোম্পানী লিমিটেড</h1>
-            <h1 className={styles.sec}>ASSAM POWER DISTRIBUTION COMPANY LIMITED</h1>
+            <h1 className={styles.sec}>
+              ASSAM POWER DISTRIBUTION COMPANY LIMITED
+            </h1>
           </div>
           <div className={styles.customerSupport}>
             <div className={styles.supportDetails}>
@@ -31,12 +33,9 @@ const Home = () => {
                   fillRule="evenodd"
                   d="M8 3a5 5 0 0 0-5 5v4.5H2V8a6 6 0 1 1 12 0v4.5h-1V8a5 5 0 0 0-5-5z"
                 ></path>
-                <path
-                  d="M11 10a1 1 0 0 1 1-1h2v4a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3zm-6 0a1 1 0 0 0-1-1H2v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-3z"
-                ></path>
+                <path d="M11 10a1 1 0 0 1 1-1h2v4a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3zm-6 0a1 1 0 0 0-1-1H2v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-3z"></path>
               </svg>
               <span> 24x7 Customer Support (1912)</span>
-              <t></t>
             </div>
             <div className={styles.supportEmail}>
               <svg
@@ -54,9 +53,7 @@ const Home = () => {
                   fillRule="evenodd"
                   d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"
                 ></path>
-                <path
-                  d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"
-                ></path>
+                <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"></path>
               </svg>
               <a href="mailto:support@apdcl.org">support@apdcl.org</a>
             </div>
@@ -64,7 +61,7 @@ const Home = () => {
         </div>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
