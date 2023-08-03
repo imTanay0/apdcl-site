03683812296223325370e24831069538c91fe314
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-08-03 19:30:35 +0530

    Fix bugs

--- client/src/utils/chartGenerator.js
+++ client/src/utils/chartGenerator.js
@@ -1,43 +1,43 @@
-// import { Chart } from 'chart.js';
-import ChartJSNodeCanvas from "chartjs-node-canvas";
+// // import { Chart } from 'chart.js';
+// import ChartJSNodeCanvas from "chartjs-node-canvas";
 
-const generateBarChartImage = async (labels, data, param) => {
-  const chartCallback = (ChartJS) => {
-    ChartJS.defaults.global.defaultFontFamily = "Arial";
-    ChartJS.defaults.global.defaultFontSize = 12;
-  };
-  const chartJSNodeCanvas = new ChartJSNodeCanvas({
-    width: 800,
-    height: 400,
-    chartCallback,
-  });
+// const generateBarChartImage = async (labels, data, param) => {
+//   const chartCallback = (ChartJS) => {
+//     ChartJS.defaults.global.defaultFontFamily = "Arial";
+//     ChartJS.defaults.global.defaultFontSize = 12;
+//   };
+//   const chartJSNodeCanvas = new ChartJSNodeCanvas({
+//     width: 800,
+//     height: 400,
+//     chartCallback,
+//   });
 
-  const configuration = {
-    type: "bar",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: param,
-          data: data,
-          backgroundColor: "aqua",
-          borderColor: "black",
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-          suggestedMax: Math.max(...data) + 1,
-        },
-      },
-    },
-  };
+//   const configuration = {
+//     type: "bar",
+//     data: {
+//       labels: labels,
+//       datasets: [
+//         {
+//           label: param,
+//           data: data,
+//           backgroundColor: "aqua",
+//           borderColor: "black",
+//           borderWidth: 1,
+//         },
+//       ],
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//           suggestedMax: Math.max(...data) + 1,
+//         },
+//       },
+//     },
+//   };
 
-  const image = await chartJSNodeCanvas.renderToDataURL(configuration);
-  return image;
-};
+//   const image = await chartJSNodeCanvas.renderToDataURL(configuration);
+//   return image;
+// };
 
-export default generateBarChartImage;
+// export default generateBarChartImage;
